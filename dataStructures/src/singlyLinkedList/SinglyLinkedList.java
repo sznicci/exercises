dca831a011664639af1534f2cb1339e525e46bd0
sznicci <sznicci@gmail.com>
2017-02-16 10:18:23 +0000

    Refactor SinglyLinkedList.java

--- dataStructures/src/singlyLinkedList/SinglyLinkedList.java
+++ dataStructures/src/singlyLinkedList/SinglyLinkedList.java
@@ -3,103 +3,97 @@ package singlyLinkedList;
 public class SinglyLinkedList {
 
 	private LinkedListNode head;
-	
+
 	public SinglyLinkedList() {
 		head = null;
 	}
-	
+
 	public SinglyLinkedList(Integer newElement) {
 		head = new LinkedListNode(newElement);
 	}
-	
+
 	public void add(Integer newElement) {
 		LinkedListNode newItem = new LinkedListNode(newElement);
 		LinkedListNode current = head;
-		
+
 		if (head == null) {
 			head = newItem;
-		} else if (head.getNext() == null) {
-			head.setNext(newItem);
-		} else {
-			while (current != null) {
-				if (current.getNext() == null) {
-					current.setNext(newItem);
-					return;
-				}
-				current = current.getNext();
-			}
+			return;
 		}
+
+		while (current.getNext() != null) {
+			current = current.getNext();
+		}
+		current.setNext(newItem);
 	}
-	
+
 	public void remove() {
 		LinkedListNode current = head;
-		
+
 		if (head == null) {
 			return;
 		} else if (head.getNext() == null) {
 			head = null;
-		} else {
-			while (current != null) {
-				if (current.getNext().getNext() == null) {
-					current.setNext(null);
-				}
-				current = current.getNext();
-			}
+			return;
 		}
+
+		while (current.getNext().getNext() != null) {
+			current = current.getNext();
+		}
+		current.setNext(null);
+
 	}
-	
+
 	public void insertAfter(Integer place, Integer element) {
 		LinkedListNode current = head;
 		Integer count = new Integer(0);
-		
+
 		if (head == null) {
 			head = new LinkedListNode(element);
-		} else {
-			while (current != null) {
-				if (count.equals(place)) {
-					LinkedListNode newItem = new LinkedListNode(element);
-					
-					newItem.setNext(current.getNext());
-					current.setNext(newItem);
-				}
-				current = current.getNext();
-				count++;
-			}
+			return;
 		}
+
+		while (!count.equals(place)) {
+			current = current.getNext();
+			count++;
+		}
+
+		LinkedListNode newItem = new LinkedListNode(element);
+
+		newItem.setNext(current.getNext());
+		current.setNext(newItem);
 	}
-	
+
 	public Integer find(Integer element) {
 		LinkedListNode current = head;
 		Integer count = new Integer(0);
-		
+
 		if (head == null) {
 			return null;
-		} else {
-			while (current != null) {
-				if (current.getValue().equals(element)) {
-					return count;
-				}
-				current = current.getNext();
-				count++;
-			}
-			return null;
 		}
+
+		while (current != null) {
+			if (current.getValue().equals(element)) {
+				return count;
+			}
+			current = current.getNext();
+			count++;
+		}
+
+		return null;
 	}
-	
+
 	public LinkedListNode getLastElement() {
 		LinkedListNode current = head;
-		
+
 		if (head == null || head.getNext() == null) {
 			return head;
-		} else {
-			while (current != null) {
-				if (current.getNext() == null) {
-					return current;
-				}
-				current = current.getNext();
-			}
-			return current;
 		}
+		
+		while (current.getNext() != null) {
+			current = current.getNext();
+		}
+		return current;
 	}
 
 	public LinkedListNode getHead() {
