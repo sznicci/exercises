d9ca0324468fa996171ca03fa67e6642c3d1473e
sznicci <sznicci@gmail.com>
2017-02-18 13:58:36 +0000

    Create Queue with Linked list

---
+++ dataStructures/src/queue/QueueWithLinkedList.java
@@ -0,0 +1,46 @@
+package queue;
+
+import doublyLinkedList.*;
+
+public class QueueWithLinkedList {
+	
+	private DoublyLinkedList list;
+	
+	public QueueWithLinkedList() {
+		list = null;
+	}
+	
+	public QueueWithLinkedList(Integer newElement) {
+		list = new DoublyLinkedList(newElement);
+	}
+	
+	public void enqueue(Integer newElement) {
+		if (list == null) {
+			list = new DoublyLinkedList(newElement);
+			return;
+		}
+		
+		list.add(newElement);
+	}
+	
+	public Integer dequeue() {
+		if (list == null) {
+			return null;
+		}
+		
+		return list.removeLast();
+	}
+	
+	public Integer examine() {
+		if (list == null) {
+			return null;
+		}
+		
+		return list.getTail().getValue();
+	}
+
+	public DoublyLinkedList getList() {
+		return list;
+	}
+	
+}
