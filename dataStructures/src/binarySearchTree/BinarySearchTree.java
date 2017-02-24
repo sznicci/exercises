89f6a9815d3a3dd9229c40a8c17594ebd197d200
sznicci <sznicci@gmail.com>
2017-02-24 14:26:17 +0000

    Add new methods and test cases

--- dataStructures/src/binarySearchTree/BinarySearchTree.java
+++ dataStructures/src/binarySearchTree/BinarySearchTree.java
@@ -1,19 +1,19 @@
 package binarySearchTree;
 
 public class BinarySearchTree<T extends Comparable<T>> {
-	
+
 	private BinarySearchTreeNode<T> root;
-	
+
 	public BinarySearchTree(T newElement) {
 		root = new BinarySearchTreeNode<T>(newElement);
 	}
-	
+
 	public void insert(BinarySearchTreeNode<T> root, T newElement) {
 		if (root == null) {
 			root = new BinarySearchTreeNode<T>(newElement);
 			return;
 		}
-		
+
 		if (root.getLeft() == null && root.getKey().compareTo(newElement) > 0) {
 			root.setLeft(new BinarySearchTreeNode<T>(newElement));
 			root.getLeft().setParent(root);
@@ -23,14 +23,74 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			root.getRight().setParent(root);
 			return;
 		}
-		
+
 		if (root.getKey().compareTo(newElement) > 0) {
 			insert(root.getLeft(), newElement);
 		} else if (root.getKey().compareTo(newElement) < 0) {
 			insert(root.getRight(), newElement);
 		}
 	}
-	
+
+	public BinarySearchTreeNode<T> search(BinarySearchTreeNode<T> root, T element) {
+		if (root == null) {
+			return null;
+		}
+
+		if (root.getKey().compareTo(element) == 0) {
+			return root;
+		} else if (root.getKey().compareTo(element) > 0) {
+			return search(root.getLeft(), element);
+		} else {
+			return search(root.getRight(), element);
+		}
+	}
+
+	public BinarySearchTreeNode<T> searchWithWhile(BinarySearchTreeNode<T> root, T element) {
+		if (root == null) {
+			return null;
+		}
+
+		while (root != null && root.getKey().compareTo(element) != 0) {
+			if (root.getKey().compareTo(element) > 0) {
+				root = root.getLeft();
+			} else if (root.getKey().compareTo(element) < 0) {
+				root = root.getRight();
+			}
+		}
+
+		return root;
+	}
+
+	public BinarySearchTreeNode<T> predecessor(BinarySearchTreeNode<T> root) {
+		if (root.getLeft() != null) {
+			return maximum(root.getLeft());
+		}
+
+		BinarySearchTreeNode<T> predesessor = root.getParent();
+
+		while (predesessor != null && predesessor.getLeft() == root) {
+			root = predesessor;
+			predesessor = predesessor.getParent();
+		}
+
+		return predesessor;
+	}
+
+	public BinarySearchTreeNode<T> successor(BinarySearchTreeNode<T> root) {
+		if (root.getRight() != null) {
+			return minimum(root.getRight());
+		}
+
+		BinarySearchTreeNode<T> successor = root.getParent();
+
+		while (successor != null && successor.getRight() == root) {
+			root = successor;
+			successor = successor.getParent();
+		}
+
+		return successor;
+	}
+
 	public BinarySearchTreeNode<T> minimum(BinarySearchTreeNode<T> root) {
 		if (root == null) {
 			return null;
@@ -39,7 +99,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return minimum(root.getLeft());
 	}
-	
+
 	public BinarySearchTreeNode<T> maximum(BinarySearchTreeNode<T> root) {
 		if (root == null) {
 			return null;
@@ -48,8 +108,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return maximum(root.getRight());
 	}
-	
-	
+
+	public String inorderToString(BinarySearchTreeNode<T> root) {
+		return ((root.getLeft() != null) ? inorderToString(root.getLeft()) : " ") + root.getKey()
+				+ ((root.getRight() != null) ? inorderToString(root.getRight()) : " ");
+	}
+
+	public String inorderToStringWithWhile(BinarySearchTreeNode<T> root) {
+		return null;
+	}
 
 	public BinarySearchTreeNode<T> getRoot() {
 		return root;
--- dataStructures/test/binarySearchTree/BinarySearchTreeTest.java
+++ dataStructures/test/binarySearchTree/BinarySearchTreeTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class BinarySearchTreeTest {
 	
 	@Test
-	public void testMinMax() {
+	public void testInorderToString() {
 		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
 		bst.insert(bst.getRoot(), 12);
 		bst.insert(bst.getRoot(), 30);
@@ -16,6 +16,89 @@ public class BinarySearchTreeTest {
 		bst.insert(bst.getRoot(), 17);
 		bst.insert(bst.getRoot(), 40);
 		
+		String expected = " 5 12 17 23 27 30 40 ";
+		
+		assertEquals("bst inordeToString sould be `expected`", expected, bst.inorderToString(bst.getRoot()));
+	}
+
+	@Test
+	public void testSearchWithWhile() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
+		bst.insert(bst.getRoot(), 12);
+		bst.insert(bst.getRoot(), 30);
+		bst.insert(bst.getRoot(), 5);
+		bst.insert(bst.getRoot(), 27);
+		bst.insert(bst.getRoot(), 17);
+		bst.insert(bst.getRoot(), 40);
+
+		assertEquals("bst search 12 key should be 12.", new Integer(12),
+				bst.searchWithWhile(bst.getRoot(), 12).getKey());
+		assertEquals("bst search 23 key should be 23.", new Integer(23),
+				bst.searchWithWhile(bst.getRoot(), 23).getKey());
+		assertEquals("bst search 17 key should be 17.", new Integer(17),
+				bst.searchWithWhile(bst.getRoot(), 17).getKey());
+		assertNull("bst search 34 key should be 12.", bst.searchWithWhile(bst.getRoot(), 34));
+	}
+
+	@Test
+	public void testSearch() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
+		bst.insert(bst.getRoot(), 12);
+		bst.insert(bst.getRoot(), 30);
+		bst.insert(bst.getRoot(), 5);
+		bst.insert(bst.getRoot(), 27);
+		bst.insert(bst.getRoot(), 17);
+		bst.insert(bst.getRoot(), 40);
+
+		assertEquals("bst search 12 key should be 12.", new Integer(12), bst.search(bst.getRoot(), 12).getKey());
+		assertEquals("bst search 23 key should be 23.", new Integer(23), bst.search(bst.getRoot(), 23).getKey());
+		assertEquals("bst search 17 key should be 17.", new Integer(17), bst.search(bst.getRoot(), 17).getKey());
+		assertNull("bst search 34 key should be 12.", bst.search(bst.getRoot(), 34));
+	}
+
+	@Test
+	public void testPredecessor() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
+		bst.insert(bst.getRoot(), 12);
+		bst.insert(bst.getRoot(), 30);
+		bst.insert(bst.getRoot(), 5);
+		bst.insert(bst.getRoot(), 27);
+		bst.insert(bst.getRoot(), 17);
+		bst.insert(bst.getRoot(), 40);
+
+		assertEquals("bst 12 predecessor key should be 5.", new Integer(5),
+				bst.predecessor(bst.getRoot().getLeft()).getKey());
+		assertEquals("bst 40 predecessor key should be 30.", new Integer(30),
+				bst.predecessor(bst.getRoot().getRight().getRight()).getKey());
+	}
+
+	@Test
+	public void testSuccessor() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
+		bst.insert(bst.getRoot(), 12);
+		bst.insert(bst.getRoot(), 30);
+		bst.insert(bst.getRoot(), 5);
+		bst.insert(bst.getRoot(), 27);
+		bst.insert(bst.getRoot(), 17);
+		bst.insert(bst.getRoot(), 40);
+
+		assertEquals("bst 12 successor key should be 17.", new Integer(17),
+				bst.successor(bst.getRoot().getLeft()).getKey());
+		assertNull("bst 40 successor key should be null.", bst.successor(bst.maximum(bst.getRoot())));
+		assertEquals("bst 27 successor key should be 30.", new Integer(30),
+				bst.successor(bst.getRoot().getRight().getLeft()).getKey());
+	}
+
+	@Test
+	public void testMinMax() {
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(23);
+		bst.insert(bst.getRoot(), 12);
+		bst.insert(bst.getRoot(), 30);
+		bst.insert(bst.getRoot(), 5);
+		bst.insert(bst.getRoot(), 27);
+		bst.insert(bst.getRoot(), 17);
+		bst.insert(bst.getRoot(), 40);
+
 		assertEquals("bst minimun key should be 5.", new Integer(5), bst.minimum(bst.getRoot()).getKey());
 		assertEquals("bst maximun key should be 40.", new Integer(40), bst.maximum(bst.getRoot()).getKey());
 	}
@@ -50,7 +133,8 @@ public class BinarySearchTreeTest {
 
 		// insert right right element
 		bst.insert(bst.getRoot(), 40);
-		assertEquals("bst root right right key should be 40.", new Integer(40), bst.getRoot().getRight().getRight().getKey());
+		assertEquals("bst root right right key should be 40.", new Integer(40),
+				bst.getRoot().getRight().getRight().getKey());
 	}
 
 }
